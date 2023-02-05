b796dd0ddaff64e04a6c6cd02460695c7f965093
Ruchit Yadav <ruchityadav955@gmail.com>
2023-02-05 12:18:00 +0530

    second commit

--- js/index.js
+++ js/index.js
@@ -21,17 +21,19 @@ function startTimer() {
     document.getElementById("stopwatch").innerHTML = minute + ":" + second + ":" + miliSecond;
 }
 
-
+// function to start the timer
 function start() {
     document.getElementById("start").disabled = true;
     timer = setInterval(startTimer, 10);
 }
 
+// function to stop the timer
 function stop() {
     document.getElementById("start").disabled = false;
     clearInterval(timer);
 }
 
+// function to reset the timer
 function reset() {
     stop();
     minute = 0;
